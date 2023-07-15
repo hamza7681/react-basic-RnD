39d3599b4a2b025646de301297b278e90877baa2
hamza <hamzambf@gmail.com>
2023-07-15 12:14:56 +0500

    issue fixed

--- src/components/Header/Drawer.jsx
+++ src/components/Header/Drawer.jsx
@@ -3,19 +3,21 @@ import { Sidebar } from "primereact/sidebar";
 
 const Drawer = ({ visible, setVisible }) => {
   return (
-    <Sidebar
-      visible={visible}
-      onHide={() => setVisible(false)}
-      showCloseIcon={false}
-    >
-      <h2>Sidebar</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-    </Sidebar>
+    <div className="md:hidden block">
+      <Sidebar
+        visible={visible}
+        onHide={() => setVisible(false)}
+        showCloseIcon={false}
+      >
+        <h2>Sidebar</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </Sidebar>
+    </div>
   );
 };
 
